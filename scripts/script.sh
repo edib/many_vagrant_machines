06e2d09b69ed7272aec961c128e76c1e4b8e321f
ibrahim edib kokdemir <kokdemir@gmail.com>
2020-04-18 18:10:20 +0300

    Update script.sh

--- scripts/script.sh
+++ scripts/script.sh
@@ -2,6 +2,8 @@ set -e -u
 
 # disable selinux
 sed -i 's/^SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
+# disable firewalld 
+systemctl disable firewalld
 # enable vagrant PasswordAuthentication
 sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
 reboot
